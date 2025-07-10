2e329103f3546cc13bb3d0242f0b487065878785
Aboje Edwin <abojeedwin@gmail.com>
2025-07-10 22:05:22 +0100

    Customer service test dependency issues resolution

--- backend/java_backend/java_backend/src/main/java/com/NextGenPay/data/model/Customer.java
+++ backend/java_backend/java_backend/src/main/java/com/NextGenPay/data/model/Customer.java
@@ -2,18 +2,14 @@ package com.NextGenPay.data.model;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-
-import java.time.LocalDateTime;
-
 
 @Getter
 @Setter
 @Document(collection = "Customer")
 public class Customer extends User{
 
-    @Id
+
     private String customerId;
 
     @NotNull(message = "This field is required")
--- backend/java_backend/java_backend/src/main/java/com/NextGenPay/data/model/UserProfile.java
+++ backend/java_backend/java_backend/src/main/java/com/NextGenPay/data/model/UserProfile.java
@@ -2,6 +2,7 @@ package com.NextGenPay.data.model;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import java.time.LocalDate;
 
@@ -11,8 +12,8 @@ import java.time.LocalDate;
 @Document(collection="UserProfile")
 public class UserProfile {
 
-    @NotNull(message = "This field is required")
-    private String userProfileId;
+    @Id
+    private String Id;
 
     @NotNull(message = "This field is required")
     private String firstName;
