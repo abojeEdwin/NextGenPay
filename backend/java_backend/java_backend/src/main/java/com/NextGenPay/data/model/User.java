129c094871d60096f6333e2ae4a8cc3de0e98ce8
Aboje Edwin <abojeedwin@gmail.com>
2025-07-10 18:29:55 +0100

    Building the services

--- backend/java_backend/java_backend/src/main/java/com/NextGenPay/data/model/User.java
+++ backend/java_backend/java_backend/src/main/java/com/NextGenPay/data/model/User.java
@@ -1,5 +1,30 @@
 package com.NextGenPay.data.model;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.intellij.lang.annotations.Identifier;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Getter
+@Setter
+@Document(collection="User")
 public class User {
+    @Id
+    private String Id;
+
+    @NotNull("This field is required")
+    private String email;
+
+    @NotNull("")
+    private String password;
+    private String phoneNumber;
+    private LocalDateTime createAt;
+    private LocalDateTime lastLogin;
 
 }
